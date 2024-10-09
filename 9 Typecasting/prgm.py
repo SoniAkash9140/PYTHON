69ac167aef285ec0b2aeff79232f65a7798df8ee
Akash Soni <as6171079@gmail.com>
2024-10-10 00:05:31 +0530

    first commit

---
+++ 9 Typecasting/prgm.py
@@ -0,0 +1,14 @@
+a="1"
+b="2"
+print(int (a)+int(b))
+
+string="15"  #explicit
+number=7
+string_number=int(string)
+sum=number+ string_number
+print("The sum of both the number is :",sum)
+
+#implicit type casting
+c=8.9
+d=2
+print(c+d)
---
+++ 9 Typecasting/typecasting.md
@@ -0,0 +1,6 @@
+Typecasting-> The conversion of from one data type to another data tye . 
+
+Type
+1) Implicit (python do itself)
+2) Explicit (user convert datatype)
+
