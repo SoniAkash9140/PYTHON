f4432d90e5f9dd1879859902f8f991eaf5e1f853
Akash Soni <as6171079@gmail.com>
2024-12-23 23:59:46 +0530

    first commit

---
+++ 14 If Else/if-else.py
@@ -0,0 +1,12 @@
+a=int(input(" Enter ur age"))
+print("Your age is : " ,a )
+
+print(a>18)
+print(a<18)
+print(a==18)
+print(a!=18)
+
+if(a>18):
+    print("You can Vote")
+else:
+    print("You Cannot Vote")
---
+++ 15 MatchCase/match.py
@@ -0,0 +1,11 @@
+a=int(input("enter the value:"))
+
+match a:
+    case 0:
+        print("X is zero")
+    case 4:
+        print("case is 4")
+    
+    case _:
+        print(a)            
+    
